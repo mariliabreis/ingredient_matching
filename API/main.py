2e002de99b6629ddddd0d039441034121879e570
marilia reis <mariliabreis@gmail.com>
2021-03-23 20:03:39 -0300

    surprise function

--- API/main.py
+++ API/main.py
@@ -13,12 +13,27 @@ ordered_keys = pd.read_csv('../raw_data/baseline_front_end/ordered_keys.csv')
 async def root():
     return {"ok": True}
 # define endpoint for searching co-occurrences with one ingredient
-@app.get("/co_occurrence/{search_word}")
-def co_occurrence(search_word):
+# get number of results according to how many there are (3% top?)
+# or maybe usar can choose how many to return 
+@app.get("/most_popular/{search_word}")
+def most_popular(search_word):
     word_index = ordered_keys[ordered_keys['replaced'] == search_word].index[0]
     ingredient_row = combination_percentage.iloc[word_index:word_index+1,:].T.sort_values(by=word_index, ascending=False)
     top_results = []
-    for index in range(1,20):
+    for index in range(1,20): # alter here to get percentage of results
+        ingr_id = ingredient_row.T.columns[index]
+        name = ordered_keys['replaced'].iloc[int(ingr_id)]
+        top_results.append(name)
+    return {'most_popular': top_results}
+
+# define function that gets a random sample of ingredients, being on the first quartile? ou maybe the whole pool of results?
+# the number of results should follow the same logic of the former endpoint
+@app.get("/feeling_adventurous/{search_word}")
+def feeling_adventurous(search_word):
+    word_index = ordered_keys[ordered_keys['replaced'] == search_word].index[0]
+    ingredient_row = combination_percentage.iloc[word_index:word_index+1,:].T.sort_values(by=word_index, ascending=False)
+    top_results = []
+    for index in range(1,20): # alter here to get percentage of results, create list of random indexes
         ingr_id = ingredient_row.T.columns[index]
         name = ordered_keys['replaced'].iloc[int(ingr_id)]
         top_results.append(name)
