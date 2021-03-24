cfb6560fcd2476ff5f6c670a13f155b564b23bd6
Giancarlo Tissot <giancarlo.tissot@gmail.com>
2021-03-24 10:29:02 -0300

    creating API

---
+++ API/main.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import scipy.sparse as sparse
+import numpy as np
+from fastapi import FastAPI
+from modules.model_processing import *
+from typing import Optional
+
+app = FastAPI()
+
+# define a root `/` endpoint
+@app.get("/")
+def index():
+    return {"Banana": 'Apple'}
+
+@app.get("/single_rec/{input_ingredient}")
+def output_func(input_ingredient: str, num_matches: Optional[int] = 15, adventure: Optional[bool] = False):
+    # item_id: int, q: Optional[str] = None
+
+    # Combines other functions into a workflow
+    num_matches += 1
+    # num_matches = 11
+
+    if type(input_ingredient) != list:
+        input_ingredient = [input_ingredient]
+    id_input = []
+
+    for ingredient in input_ingredient:
+        id_input.append(get_id(ingredient))
+    id_list = find_match(id_input,num_matches)
+    names = list_to_names(id_list)
+    names.pop(0)
+    return {'Recommendations': names}
+
+# @app.get("/mult_rec/{input_ingredient}")
+# def output_func(input_ingredient: str):
