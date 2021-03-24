35c0986668a8218eadd99928fe174c1af4633ac2
marilia reis <mariliabreis@gmail.com>
2021-03-24 12:57:16 -0300

    organizing for pull request - API compatibilization

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from typing import Optional
-from model import word2vec
+from model import word2vec, most_popular, surprise
+# other imports?
 
 app = FastAPI()
 
@@ -16,3 +17,15 @@ def index():
 def read_item(ingredient: str):
   ms = word2vec(ingredient)
   return {"most_similar": ms}
+
+# gets most popular combinations with one ingredient
+@app.get("/most_popular/{ingredient}")
+def read_item(ingredient: str):
+  mp = most_popular(ingredient)
+  return {"most_popular": mp}
+
+# gets surprising combinations with one ingredient, considering only the third top tier
+@app.get("/surprise/{ingredient}")
+def read_item(ingredient: str):
+  sur = most_popular(ingredient)
+  return {"surprise": sur}
--- api/model.py
+++ api/model.py
@@ -1,6 +1,9 @@
 import joblib
 import pandas as pd
 
+combination_percentage = pd.read_csv('../raw_data/baseline_front_end/combination_percentage.csv')
+ordered_keys = pd.read_csv('../raw_data/baseline_front_end/ordered_keys.csv')
+
 def word2vec(ingredient):
 
   word2vec = joblib.load('models/word2vec.joblib')
@@ -9,3 +12,27 @@ def word2vec(ingredient):
   except:
     ms = {'ERROR': f'We are sorry but the word "{ingredient}" is not in vocabulary... try eating something else! :)'}
   return ms
+
+# gets most popular combinations with one ingredient
+def most_popular(search_word):
+    word_index = ordered_keys[ordered_keys['replaced'] == search_word].index[0]
+    ingredient_row = combination_percentage.iloc[word_index:word_index+1,:].T.sort_values(by=word_index, ascending=False)
+    top_results = []
+    for index in range(1,20): # alter here to get percentage of results
+        ingr_id = ingredient_row.T.columns[index]
+        name = ordered_keys['replaced'].iloc[int(ingr_id)]
+        top_results.append(name)
+    return top_results
+
+# gets surprising combinations with one ingredient, considering only the third top tier
+def surprise(search_word):
+    word_index = ordered_keys[ordered_keys['replaced'] == search_word].index[0]
+    ingredient_row = combination_percentage.iloc[word_index:word_index+1,:].T.sort_values(by=word_index, ascending=False)
+    size_results = ingredient_row[ingredient_row[word_index] != 0].shape[0]
+    random_toptier = []
+    for index in np.random.randint(0,int(size_results/3),20):
+        ingr_id = ingredient_row.T.columns[index]
+        name = ordered_keys['replaced'].iloc[ingr_id]
+        random_toptier.append(name)
+
+    return random_toptier
